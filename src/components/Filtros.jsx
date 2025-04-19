7f907ef00aa1535c264be95146acc6b28182c317
Leonardo1599 <jrios.231ds28@istta.edu.pe>
2025-04-19 18:33:26 -0500

    Aumento de categorías para el filtrado de datos

--- src/components/Filtros.jsx
+++ src/components/Filtros.jsx
@@ -17,9 +17,51 @@ const Filtros = ({ onFilterChange }) => {
         </select>
         <select name="category" onChange={handleFilterChange}>
           <option value="">Categoría</option>
-          <option value="shooter">Shooter</option>
           <option value="mmorpg">MMORPG</option>
-          <option value="fantasy">Fantasía</option>
+          <option value="shooter">Shooter</option>
+          <option value="strategy">Strategy</option>
+          <option value="moba">MOBA</option>
+          <option value="racing">Racing</option>
+          <option value="sports">Sports</option>
+          <option value="social">Social</option>
+          <option value="sandbox">Sandbox</option>
+          <option value="open-world">Open World</option>
+          <option value="survival">Survival</option>
+          <option value="pvp">PvP</option>
+          <option value="pve">PvE</option>
+          <option value="pixel">Pixel</option>
+          <option value="voxel">Voxel</option>
+          <option value="zombie">Zombie</option>
+          <option value="turn-based">Turn-Based</option>
+          <option value="first-person">First Person</option>
+          <option value="third-person">Third Person</option>
+          <option value="top-down">Top-Down</option>
+          <option value="tank">Tank</option>
+          <option value="space">Space</option>
+          <option value="sailing">Sailing</option>
+          <option value="side-scroller">Side Scroller</option>
+          <option value="superhero">Superhero</option>
+          <option value="permadeath">Permadeath</option>
+          <option value="card">Card</option>
+          <option value="battle-royale">Battle Royale</option>
+          <option value="mmo">MMO</option>
+          <option value="mmofps">MMOFPS</option>
+          <option value="mmotps">MMOTPS</option>
+          <option value="3d">3D</option>
+          <option value="2d">2D</option>
+          <option value="anime">Anime</option>
+          <option value="fantasy">Fantasy</option>
+          <option value="sci-fi">Sci-Fi</option>
+          <option value="fighting">Fighting</option>
+          <option value="action-rpg">Action-RPG</option>
+          <option value="action">Action</option>
+          <option value="military">Military</option>
+          <option value="martial-arts">Martial Arts</option>
+          <option value="flight">Flight</option>
+          <option value="low-spec">Low-Spec</option>
+          <option value="tower-defense">Tower Defense</option>
+          <option value="horror">Horror</option>
+          <option value="mmorts">MMORTS</option>
         </select>
         <select name="sort-by" onChange={handleFilterChange}>
           <option value="">Ordenar por</option>
